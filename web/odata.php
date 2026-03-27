95a1e66a85a376fb7510505332b63ab79a7be402
Tim Falken <tfalken@kvt.nl>
2026-03-27 12:56:52 +0100

    fix odata status errors

--- web/odata.php
+++ web/odata.php
@@ -759,9 +759,6 @@ function odata_send_cache_status_json(): void
         xdebug_disable();
     }
 
-    require_once __DIR__ . '/auth.php';
-    require_once __DIR__ . '/logincheck.php';
-
     header('Content-Type: application/json; charset=utf-8');
     header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
 
@@ -775,9 +772,6 @@ function odata_send_cache_delete_json(): void
     if (function_exists('xdebug_disable')) {
         xdebug_disable();
     }
-
-    require_once __DIR__ . '/auth.php';
-    require_once __DIR__ . '/logincheck.php';
 
     header('Content-Type: application/json; charset=utf-8');
     header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
@@ -813,9 +807,6 @@ function odata_send_cache_clear_json(): void
     if (function_exists('xdebug_disable')) {
         xdebug_disable();
     }
-
-    require_once __DIR__ . '/auth.php';
-    require_once __DIR__ . '/logincheck.php';
 
     header('Content-Type: application/json; charset=utf-8');
     header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
